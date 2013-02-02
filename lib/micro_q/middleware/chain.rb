eaede31e070c305e23e7a520474e12c7193a4e5b
Brian Norton <brian.nort@gmail.com>
2013-02-02 06:13:11 -0800

    #add_before and #add_after now supported for middleware. Adding documentation for the middleware chain.

--- lib/micro_q/middleware/chain.rb
+++ lib/micro_q/middleware/chain.rb
@@ -2,11 +2,33 @@ require 'micro_q/middleware/server/retry'
 
 module MicroQ
   module Middleware
+    ##
+    # An Array wrapper that holds the class name of middlewares to call
+    # around the execution of messages. The most basic middleware must
+    # yield to the given block to allow the message to be invoked. Not
+    # yielding causes the message to be dropped and not invoked.
+    #
+    # A minimal middleware:
+    # class MyFunMiddleware
+    #   def call(worker, message)
+    #     # Do something fun here ...
+    #     yield
+    #     # More fun goes here ...
+    #   end
+    # end
+    #
     class Chain
+      ##
+      # Middleware chain that is run around execution of messages.
+      #   -- If halted, the message will not be invoked.
       def server
         @server ||= Server.new
       end
 
+      ##
+      # Middleware chain that is run around message push.
+      #   -- If halted, the message will not enter the queue.
+      #
       def client
         @client ||= Client.new
       end
@@ -15,19 +37,46 @@ module MicroQ
         attr_reader :entries
 
         def initialize
-          @entries = []
+          clear
         end
 
+        ##
+        # Add any number of entries to the middleware chain
+        #
         def add(*items)
           @entries.concat(items.flatten).uniq!
         end
 
+        def add_before(before, *items)
+          remove(*items)
+          @entries.insert(@entries.index(before), *items).uniq! if items.any?
+        end
+
+        def add_after(after, *items)
+          remove(*items)
+          @entries.insert(@entries.index(after)+1, *items).uniq! if items.any?
+        end
+
+        ##
+        # Remove any number of entries from the middleware chain
+        #
         def remove(*items)
           @entries.tap do
             items.flatten.each {|item| @entries.delete(item) }
           end
         end
 
+        def clear
+          @entries = []
+        end
+
+        ##
+        # Traverse the middleware chain by recursing until we reach the
+        # end of the chain and are able to invoke the given block. The block
+        # represents a message push (client) or a message invocation (server).
+        #
+        # It is, however very generic and can be used as middleware around anything.
+        #
         def call(*args, &block)
           chain = (index = -1) && -> {
             (index += 1) == entries.length ?
--- spec/lib/middleware/chain_spec.rb
+++ spec/lib/middleware/chain_spec.rb
@@ -4,9 +4,10 @@ describe MicroQ::Middleware::Chain do
   describe MicroQ::Middleware::Chain::Base do
     subject { MicroQ::Middleware::Chain::Base.new }
 
-    describe '#add' do
-      class MyMiddleware; end
+    class MyMiddleware; end
+    class OtherMiddleware; end
 
+    describe '#add' do
       before do
         subject.add MyMiddleware
       end
@@ -28,8 +29,6 @@ describe MicroQ::Middleware::Chain do
     end
 
     describe '#remove' do
-      class MyMiddleware; end
-
       before do
         subject.add MyMiddleware
 
@@ -39,6 +38,159 @@ describe MicroQ::Middleware::Chain do
 
       it 'should remove the item' do
         subject.entries.should_not include(MyMiddleware)
+      end
+    end
+
+    describe '#clear' do
+      before do
+        subject.add MyMiddleware
+      end
+
+      it 'should remove all entries' do
+        subject.entries.should == [MyMiddleware]
+        subject.clear
+
+        subject.entries.should == []
+      end
+    end
+
+    describe '#add_before' do
+      class MiddleMiddleware; end
+
+      def add_before
+        subject.add_before(OtherMiddleware, MiddleMiddleware)
+      end
+
+      before do
+        subject.clear
+        subject.add MyMiddleware
+        subject.add OtherMiddleware
+      end
+
+      it 'should not add nils' do
+        subject.add_before MyMiddleware
+        subject.entries.should have(2).items
+      end
+
+      it 'should add the middleware in the specified place' do
+        add_before
+
+        subject.entries.should == [
+          MyMiddleware,
+          MiddleMiddleware,
+          OtherMiddleware
+        ]
+      end
+
+      describe 'when relocating middleware' do
+        before do
+          subject.add MiddleMiddleware
+        end
+
+        it 'should move the middleware to the specified place' do
+          subject.entries.should == [
+            MyMiddleware,
+            OtherMiddleware,
+            MiddleMiddleware
+          ]
+
+          add_before
+
+          subject.entries.should == [
+            MyMiddleware,
+            MiddleMiddleware,
+            OtherMiddleware
+          ]
+        end
+      end
+
+      describe 'when adding multiple' do
+        class SecondMiddleMiddleware; end
+
+        def add_before
+          subject.add_before(OtherMiddleware, MiddleMiddleware, SecondMiddleMiddleware)
+        end
+
+        it 'should add the middlewares in the specified place' do
+          add_before
+
+          subject.entries.should == [
+            MyMiddleware,
+            MiddleMiddleware,
+            SecondMiddleMiddleware,
+            OtherMiddleware
+          ]
+        end
+      end
+    end
+
+    describe '#add_after' do
+      class MiddleMiddleware; end
+
+      def add_after
+        subject.add_after(OtherMiddleware, MiddleMiddleware)
+      end
+
+      before do
+        subject.clear
+        subject.add MyMiddleware
+        subject.add OtherMiddleware
+      end
+
+      it 'should not add nils' do
+        subject.add_after MyMiddleware
+        subject.entries.should have(2).items
+      end
+
+      it 'should add the middleware in the specified place' do
+        add_after
+
+        subject.entries.should == [
+          MyMiddleware,
+          OtherMiddleware,
+          MiddleMiddleware
+        ]
+      end
+
+      describe 'when relocating middleware' do
+        before do
+          subject.add_before MyMiddleware, MiddleMiddleware
+        end
+
+        it 'should move the middleware to the specified place' do
+          subject.entries.should == [
+            MiddleMiddleware,
+            MyMiddleware,
+            OtherMiddleware
+          ]
+
+          add_after
+
+          subject.entries.should == [
+            MyMiddleware,
+            OtherMiddleware,
+            MiddleMiddleware
+          ]
+        end
+      end
+
+      describe 'when adding multiple' do
+        class SecondMiddleMiddleware; end
+
+        def add_after
+          subject.add_after(OtherMiddleware, MiddleMiddleware, SecondMiddleMiddleware)
+        end
+
+        it 'should add the middlewares in the specified place' do
+          add_after
+
+          subject.entries.should == [
+            MyMiddleware,
+            OtherMiddleware,
+            MiddleMiddleware,
+            SecondMiddleMiddleware
+          ]
+        end
       end
     end
   end
