7524a2628e73bd7526848bb84a8ad69cb46c8150
Brian Norton <brian.nort@gmail.com>
2013-02-08 21:49:19 -0800

    Show how user defined managers would work.

--- lib/micro_q/manager.rb
+++ lib/micro_q/manager.rb
@@ -1,1 +1,16 @@
 require 'micro_q/manager/default'
+
+##
+# The Manager interface
+#
+# The concrete manager implementation encapsulates a queue and
+# a pool of workers. The worker pool need only be a collection of
+# workers that can accept and perform messages.
+#
+# :method: start
+#   - Begin the run loop for message processing.
+#
+# Note that the default manager is a celluloid actor that reschedules
+# itself (via the after(seconds) { start }) call which is a recursive
+# call that executes asynchronously. This behavior is critical.
+#
