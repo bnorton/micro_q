8c284fbd49ac49b813145c96d8a78f646e044db2
Brian Norton <brian.nort@gmail.com>
2013-02-08 21:50:37 -0800

    Define the worker interface with examples.

--- lib/micro_q/worker.rb
+++ lib/micro_q/worker.rb
@@ -1,1 +1,17 @@
 require 'micro_q/worker/standard'
+
+##
+# The Worker interface
+#
+# The concrete worker implementation is the member of the chain that
+# actually invokes the middleware chain and executes/performs the given
+# message.
+#
+# :method: perform
+#   - Re-hydrates and invokes the methods that the message specifies.
+#   - :args: (message)
+#     message is the hash the represents an pushed item
+#
+# Based on what keys are pushed to and de-queued from the queue,
+# do the actions that the message specifies.
+#
